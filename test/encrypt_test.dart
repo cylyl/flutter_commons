3f1a29b7bb19e6cb4e2eb8ad6a825b3ec41b64f2
chung2012 <chungyl2012@gmail.com>
2024-03-15 14:50:56 +0800

    update test

--- test/encrypt_test.dart
+++ test/encrypt_test.dart
@@ -1,9 +1,7 @@
-import 'package:encrypt/encrypt.dart' as e;
 import 'package:flutter_commons/flutter_commons.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  Encrypt encrypt = Encrypt.instance;
   final String salt = '[wfS*]-%afhkDg4aCyv}vC;aDWk5aM?:';
   void setup() async {}
 
@@ -12,18 +10,13 @@ void main() {
 
     String s = "test";
 
-    String b64 = encrypt.encrypt(salt, s)!;
-
-    final key = e.Key.fromUtf8(salt);
-    final iv = e.IV.fromLength(16);
-
-    final encrypter = e.Encrypter(e.AES(key));
-
-    final decrypted = encrypter.decrypt64(b64, iv: iv);
-
+    Encrypt encrypt = Encrypt.instance;
+    String? b64 = encrypt.encrypt(salt, s);
+    String? decrypted = encrypt.decrypt(salt, b64!);
     print(decrypted);
     print(b64);
 
     assert(decrypted == s);
+
   });
 }
